dc82124876b0fd782de63af016f96167810896c1
Chance Vodnoy <chancevodnoy129@gmail.com>
2025-11-14 21:50:49 -0800

    Updated openmp again

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -28,7 +28,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    #pragma omp parallel for
    for (int i = 0; i < n; i++)
    {
-      int sum = 0;
+      double sum = 0.0;
       for (int j = 0; j < n; j++)
       {
          sum += A[i * n + j] * x[j];
